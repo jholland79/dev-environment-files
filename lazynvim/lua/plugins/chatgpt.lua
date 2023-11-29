d77082b7f2f3fa7b592b22032fd3f77c59dd4d41
jholland79 <john@hollandit.ie>
2023-11-29 21:55:50 +0000

    Disabled ChatGPT

--- lazynvim/lua/plugins/chatgpt.lua
+++ lazynvim/lua/plugins/chatgpt.lua
@@ -1,17 +1,21 @@
 -- lazy.nvim
-return {
+if true then
+  return {}
+else
+  return {
 
-  "robitx/gp.nvim",
-  config = function()
-    local conf = {
-      -- required openai api key
-      openai_api_key = os.getenv("OPENAI_API_KEY"),
-    }
-    require("gp").setup(conf)
+    "robitx/gp.nvim",
+    config = function()
+      local conf = {
+        -- required openai api key
+        openai_api_key = os.getenv("OPENAI_API_KEY"),
+      }
+      require("gp").setup(conf)
 
-    -- or setup with your own config (see Install > Configuration in Readme)
-    -- require("gp").setup(conf)
+      -- or setup with your own config (see Install > Configuration in Readme)
+      -- require("gp").setup(conf)
 
-    -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
-  end,
-}
+      -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
+    end,
+  }
+end
